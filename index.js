da40db5fd6ba35852c6ea22a7f7bb610ba9d5049
Daniel Fernandes <danielgiovane2015@gmail.com>
2020-09-29 16:07:47 -0300

    Versão 0.1, Iniciando projeto e criando a estrutura da aplicação

---
+++ index.js
@@ -0,0 +1,11 @@
+const url = `https://api.lyrics.ovh`;
+
+const form = document.querySelector('.form');
+const inputSearch = document.querySelector('.search');
+const btn = document.querySelector('.busca-btn');
+const musicasContainer = document.querySelector('.musicas-container');
+const anteriorEProximo = document.querySelector('.anterior-e-proximo');
+
+console.log(form,inputSearch,btn,musicasContainer,anteriorEProximo)
+
+
