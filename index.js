7f13cd416168b75e371fc05e70e1a72dcd603941
Daniel Fernandes <danielgiovane2015@gmail.com>
2020-09-29 17:57:03 -0300

    Versão 0.6, Criando função para exibir o conteúdo na tela.

--- index.js
+++ index.js
@@ -6,12 +6,22 @@ const btn = document.querySelector('.busca-btn');
 const musicasContainer = document.querySelector('.musicas-container');
 const anteriorEProximo = document.querySelector('.anterior-e-proximo');
 
+const inserindoMusicasNaPagina = (musicas) => {
+  musicasContainer.innerHTML = musicas.data.map(musica => 
+    ` 
+    <li class="musica"> <strong> ${musica.artist.name} </strong> - ${musica.title}</li>
+    <button class="busca-btn">Veja Letra</button>
+
+    `).join('');
+}
 
 const fetchLetraDeMusicas = async (termo) => {
   const resposta = await fetch(`${url}/suggest/${termo}`);
   const respostaJson = resposta.ok ? resposta.json() : Promise.reject(statusText);
-  const dados = await respostaJson; 
-  console.log(dados)
+  const dados = await respostaJson;
+
+  inserindoMusicasNaPagina(dados)
+
 }
 
 
