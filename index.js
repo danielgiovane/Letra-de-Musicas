81d8e20997bcb5861792a1b18edd86e121f95bbe
Daniel Fernandes <danielgiovane2015@gmail.com>
2020-09-29 16:42:52 -0300

    Versão 0.4, Implementando metodo fetchLetrasDeMusicas e handleDados

--- index.js
+++ index.js
@@ -6,13 +6,24 @@ const btn = document.querySelector('.busca-btn');
 const musicasContainer = document.querySelector('.musicas-container');
 const anteriorEProximo = document.querySelector('.anterior-e-proximo');
 
-form.addEventListener('submit', function(e) {
+
+const handleDados = res => res.ok ? res.json() : Promise.reject(statusText);
+
+const fetchLetraDeMusicas = (termo) => {
+  fetch(`${url}/suggest/${termo}`)
+    .then(handleDados)
+}
+
+
+form.addEventListener('submit', function (e) {
   e.preventDefault();
 
   const termoDaBusca = inputSearch.value.trim();
-  if(!termoDaBusca){
+  if (!termoDaBusca) {
     musicasContainer.innerHTML = `<li class="aviso-vermelho">Por favor digite um termo valido</li>`
     return
   }
+
+  fetchLetraDeMusicas(termoDaBusca);
 
 })
